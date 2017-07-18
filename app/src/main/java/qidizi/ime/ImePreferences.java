9fca8f11acc9c1ef9bfd69719d3ea0112ad1bcee
qidizi <qidizi@qq.com>
2017-07-19 00:07:58 +0800

    完成键盘字符生成

--- app/src/main/java/qidizi/ime/ImePreferences.java
+++ app/src/main/java/qidizi/ime/ImePreferences.java
@@ -10,43 +10,45 @@ import android.preference.PreferenceActivity;
  * Displays the IME preferences inside the input method setting.
  */
 public class ImePreferences extends PreferenceActivity {
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		//修改top activity的标题bar成可以点击返回的功能
-	    ActionBar actionBar = this.getActionBar();
-	    actionBar.setDisplayHomeAsUpEnabled(true);
-	};
-	/*
-	 * 修改启动intent，让总是启动器设置activity
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //修改top activity的标题bar成可以点击返回的功能
+        ActionBar actionBar = this.getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    ;
+    /*
+     * 修改启动intent，让总是启动器设置activity
 	 */
 
-	@Override
-	public Intent getIntent() {
-		final Intent modIntent = new Intent(super.getIntent());
-		modIntent.putExtra(EXTRA_SHOW_FRAGMENT, Settings.class.getName());
-		modIntent.putExtra(EXTRA_NO_HEADERS, true);
-		return modIntent;
-	}
+    @Override
+    public Intent getIntent() {
+        final Intent modIntent = new Intent(super.getIntent());
+        modIntent.putExtra(EXTRA_SHOW_FRAGMENT, Settings.class.getName());
+        modIntent.putExtra(EXTRA_NO_HEADERS, true);
+        return modIntent;
+    }
 
-	/*
-	 * setings class exists
-	 */
-	@Override
-	protected boolean isValidFragment(final String fragmentName) {
-		return Settings.class.getName().equals(fragmentName);
-	}
+    /*
+     * setings class exists
+     */
+    @Override
+    protected boolean isValidFragment(final String fragmentName) {
+        return Settings.class.getName().equals(fragmentName);
+    }
 
-	/*
-	 * 内部类
-	 */
-	public static class Settings extends InputMethodSettingsFragment {
-		@Override
-		public void onCreate(Bundle savedInstanceState) {
-			super.onCreate(savedInstanceState);
-			setSubtypeEnablerTitle(R.string.select_language);
-			//append自定义配置项目
-			addPreferencesFromResource(R.xml.ime_preferences);
-		}
-	}
+    /*
+     * 内部类
+     */
+    public static class Settings extends InputMethodSettingsFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setSubtypeEnablerTitle(R.string.select_language);
+            //append自定义配置项目
+            addPreferencesFromResource(R.xml.ime_preferences);
+        }
+    }
 }
--- app/src/main/java/qidizi/ime/KeyBoradView.java
+++ app/src/main/java/qidizi/ime/KeyBoradView.java
@@ -1,10 +1,14 @@
 package qidizi.ime;
 
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.graphics.Typeface;
+import android.os.DeadObjectException;
+import android.util.DisplayMetrics;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -21,6 +25,14 @@ public class KeyBoradView extends View  {
 	 * 列数
 	 */
 	private static final int cells = 10;
+	/**
+	 * 中间大字textsize
+	 */
+	private static int centerFontSize = 0;
+	/**
+	 * 边上的字textsize
+	 */
+	private static int sideFontSize = 0;
 
 	/**
 	 * 字
@@ -40,9 +52,12 @@ public class KeyBoradView extends View  {
 	 */
 	@Override
 	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-		keySize = getWidth() / cells;
+        // 上面二个值是建议，最大值
+        int width  = MeasureSpec.getSize(widthMeasureSpec);
+        keySize = Math.round(width / cells);
+        _getFontSize();
 		// 通知父类，新的大小
-		setMeasuredDimension(widthMeasureSpec, keySize * rows);
+		setMeasuredDimension(width, keySize * rows);
 	}
 
 	/**
@@ -67,67 +82,73 @@ public class KeyBoradView extends View  {
 			canvas.drawLine(0,i * keySize,width,i * keySize,paint);
 		}
 
-		// 画字
+        paint.setColor(Color.RED);
+        canvas.drawRect(5,2,100,100,paint);
+		// 画字,x->为正；y↓为正；view的左上角为（0，0）
 
 		// 每键4角 + 4边 + 1中间 = 9字
 		int sum = cells * rows * 9;
 		String str =  "0";
-		// paint.setStyle(Paint.Style.STROKE);
 		paint.setTypeface(Typeface.MONOSPACE);
-		// 让x=0是字中间处于viewleft边上，正数是右移
+		// drawtext的x指字中心离原点距离，y却是指字底边到原点距离（并非中心）
 		paint.setTextAlign(Paint.Align.CENTER);
 		Typeface typeface = Typeface.createFromAsset(getContext().getAssets(), "keys.ttf");
 		paint.setTypeface(typeface);
 		paint.setColor(Color.WHITE);
-		int fontHeight = (int) (paint.descent() - paint.ascent());
-		// 当y=0 => yb=y+abs(baseline - 字高/2);
-		int baseline = Math.abs((int) paint.ascent());
+        // 字中心点大概到y点，大概是0.4个字高
+        double x = 0,y = 0, baseLine = 0.4;
 
 		for (int row = 1; row <= rows; row++) {
 			for (int cell = 1; cell <= cells; cell++) {
 				for (int i = 1; i <= 9; i++) {
 					// 键字顺序为左角，上边，右角，右边，右下角，下边，左下角，中间
 					str = "" + i;
-
-					int x = 0;
-					int y = 0;
+                    paint.setTextSize(sideFontSize);
 
 					switch (i){
 						case 1:
-							x = keySize * (cell - 1);
-							y = keySize * (row -1);
+						    // x右移半个字
+							x = keySize * (cell - 1) + sideFontSize * 0.5;
+                            // y，中心点下移半个字高
+							y = keySize * (row -1) + sideFontSize * (0.5 + baseLine);
 							break;
 						case 2:
-							x = (int)( keySize * (cell - 1 + 0.5) );
-							y = keySize * (row - 1);
+                            x = keySize * (cell - 1 + 0.5) + sideFontSize * 0.5;
+                            // y，中心点下移半个字高
+                            y = keySize * (row -1) + sideFontSize * (0.5 + baseLine);
 							break;
 						case 3:
-							x = keySize * cell - fontHeight / 2;
-							y = keySize * (row - 1);
+							x = keySize * cell - sideFontSize * 0.5;
+                            // y，中心点下移半个字高
+                            y = keySize * (row -1) + sideFontSize * (0.5 + baseLine);
 							break;
 						case 4:
-							x = keySize * cell - fontHeight / 2;
-							y = (int)( keySize * (row - 1 + 0.5) );
+                            x = keySize * cell - sideFontSize * 0.5;
+                            y = keySize * (row -1 + 0.5);
 							break;
 						case 5:
-							x = keySize * cell - fontHeight / 2;
-							y = keySize * row - fontHeight;
+                            x = keySize * cell - sideFontSize * 0.5;
+							y = keySize * row - sideFontSize * 05;
 							break;
 						case 6:
-							x = (int)( keySize * (cell - 1 + 0.5) );
-							y = keySize * row - fontHeight;
+                            x = keySize * (cell - 1 + 0.5) + sideFontSize * 0.5;
+                            y = keySize * row - sideFontSize * 05;
 							break;
 						case 7:
-							x = keySize * (cell - 1);
-							y = keySize * row - fontHeight;
+                            x = keySize * (cell - 1) + sideFontSize * 0.5;
+                            y = keySize * row - sideFontSize * 05;
 							break;
 						case 8:
-							x = keySize * (cell - 1);
-							y = (int)( keySize * (row - 1 + 0.5) );
+                            x = keySize * (cell - 1) + sideFontSize * 0.5;
+                            y = keySize * (row -1 + 0.5);
 							break;
 						case 9:
-							x = (int)( keySize * (cell - 1 + 0.5) ) - fontHeight;
-							y = (int)( keySize * (row - 1 + 0.5) ) - fontHeight;
+						    // 中间那个字
+                            paint.setTextSize(centerFontSize);
+                            // 半个键
+							x = keySize * (cell - 1 + 0.5);
+                            // 向下移半个字高的4/10
+							y = keySize * (row - 1 + 0.5) + centerFontSize * baseLine;
 							break;
 					}
 
@@ -148,9 +169,8 @@ public class KeyBoradView extends View  {
 						str =  "\0";
 					}
 
-					float baselineY = y + baseline - fontHeight / 2;
 					// x在笔刷初始时已经设定成中心为0点;
-					canvas.drawText(str, x, baselineY, paint);
+					canvas.drawText(str, (float)x, (float)y, paint);
 				}
 			}
 		}
@@ -158,6 +178,64 @@ public class KeyBoradView extends View  {
 		paint = null;// 释放
 	}
 
+	/**
+	 * 计算键位上边与中心字大小
+	 * 一般字体都是高比宽大，所以，只需要考虑高度合适即可
+	 */
+	private void _getFontSize () {
+		//目前不清楚机制，如果已经设置了，就没有必要再重新计算
+		if (centerFontSize > 0) {
+			return;
+		}
+
+		// 在26个字母拼写中，小写f占用4行
+		String s = "f";
+		// 中心那个字的高度占整个键比例
+		double bigHeight = 0.5;
+		Paint p = new Paint();
+		Rect bounds = new Rect();
+		int fontHeight = (int)Math.round(keySize * bigHeight);
+		int height = 0;
+
+		// 先计算中间字高度
+		for (int size = 1; fontHeight > height; size++) {
+			// 不停的调整size，计算单字高度
+			p.setTextSize(size);
+			p.getTextBounds(s, 0, s.length(), bounds);
+			int tmp = bounds.height();
+
+			if (tmp > fontHeight) {
+				// 已经超出了，就取前面的值
+				break;
+			}
+
+			height = tmp;
+		}
+
+		centerFontSize = height;
+		// 重设
+		height = 0;
+        fontHeight = (int)Math.round(keySize * (1 - bigHeight) / 2);
+
+
+        // 先计算边上字高度
+		for (int size = 1; fontHeight > height; size++) {
+			// 不停的调整size，计算单字高度
+			p.setTextSize(size);
+			p.getTextBounds(s, 0, s.length(), bounds);
+			int tmp = bounds.height();
+
+			if (tmp > fontHeight) {
+				// 已经超出了，就取前面的值
+				break;
+			}
+
+			height = tmp;
+		}
+
+		sideFontSize = height;
+        p = null;
+	}
 
 	/**
 	 * 按键只需要接click；和移动
